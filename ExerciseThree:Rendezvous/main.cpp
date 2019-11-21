f671455730aff74e6ccdf17aa68f20628b4332bc
williamnolan97 <williamnolan97@gmail.com>
2019-11-21 10:50:29 +0000

    Lab 3

--- ExerciseThree:Rendezvous/main.cpp
+++ ExerciseThree:Rendezvous/main.cpp
@@ -13,14 +13,18 @@
 void taskOne(std::shared_ptr<Semaphore> firstSem,std::shared_ptr<Semaphore>  secondSem, int delay){
   std::this_thread::sleep_for(std::chrono::seconds(delay));
   std::cout <<"Task One has arrived! "<< std::endl;
-  //THIS IS THE RENDEZVOUS POINT!
+  firstSem->Signal();
+  //Rendezvous point
+  secondSem->Wait();
   std::cout << "Task One has left!"<<std::endl;
 }
 /*! displays a message that is split in to 2 sections to show how a rendezvous works*/
 void taskTwo(std::shared_ptr<Semaphore> firstSem, std::shared_ptr<Semaphore> secondSem, int delay){
   std::this_thread::sleep_for(std::chrono::seconds(delay));
   std::cout <<"Task Two has arrived "<<std::endl;
-  //THIS IS THE RENDEZVOUS POINT!
+  secondSem->Signal();
+  //Rendezvous point
+  firstSem->Wait();
   std::cout << "Task Two has left "<<std::endl;
 }
 
